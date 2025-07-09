8aa9cbfd2449a3c333281264e959aef11c6c65c8
AldanaPortugalPickling <aldanapickling@gmail.com>
2025-07-09 16:46:36 -0500

    Made adjustments to the code

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //This is journal project week 02
         Console.WriteLine("Hello World! This is the Journal Project.");
     }
 }
