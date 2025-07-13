5d4bf64cd1131d8105ec277a7d6a22db1286ade0
AldanaPortugalPickling <aldanapickling@gmail.com>
2025-07-13 07:23:04 -0500

    Finished week01/exercise3.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string answer = "yes";
+
+        while (answer == "yes")
+        {
+            //Ask the computer a random number.
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guessNumber = -1;
+
+            Console.WriteLine("Guess the magic number between 1 and 100!");
+
+            //Added a loop and if statements:
+
+            while (magicNumber != guessNumber)
+            {
+                Console.WriteLine("What is your guess ? ");
+                guessNumber = int.Parse(Console.ReadLine()); //Convert string to number.
+
+
+                if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine("Do you want to play again? ");
+            answer = Console.ReadLine();
+
+        }
+
+        Console.WriteLine("Thanks for playing!");
+
     }
 }
