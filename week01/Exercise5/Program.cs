dc6406a8f21b1864d48adace0de17d8815de5e8e
AldanaPortugalPickling <aldanapickling@gmail.com>
2025-07-13 18:51:08 -0500

    xd

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+
+
+
+        //Execute the sum:
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        //The average of the numbers in the list:
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Find the maximun or largest number in the list.
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
+
     }
 }
