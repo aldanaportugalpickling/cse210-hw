6767af6efc53bba4221cf023ac2ca0e68bc0c5b8
AldanaPortugalPickling <aldanapickling@gmail.com>
2025-07-13 18:50:13 -0500

    Finished week01/exercise4.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,76 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string text = Console.ReadLine();
+            userNumber = int.Parse(text);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+
+        //Execute the sum:
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        //The average of the numbers in the list:
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Find the maximun or largest number in the list.
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
+
+        //Find the minimun positive number: 
+        //Optional.
+        int minPositive = int.MaxValue;
+
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < minPositive)
+            {
+                minPositive = number;
+            }
+        }
+
+        Console.WriteLine($"The smallest positive number is: {minPositive}");
+
+        //sorted list.
+
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
     }
 }
